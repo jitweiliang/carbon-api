d0f2ff3db7d9d86ba538cf674c393bff3accdb62
ReturnLegacy2626 <92350939+ReturnLegacy2626@users.noreply.github.com>
2024-03-25 20:02:07 +0800

    emission and reset up glould deploy

--- src/controllers/EmissionController.php
+++ src/controllers/EmissionController.php
@@ -29,7 +29,7 @@
 
                             $stmt = "select t1.id as id, t1.user_id as userId, t2.user_name as userName,
                                         t1.submitted_date as submittedDate, t1.household as household, t1.transportation as transportation,
-                                        t1.food as food, t1.total_emission as totalEmission, t1.submittedDate as submittedDate
+                                        t1.food as food, t1.total_emission as totalEmission 
                                         from carbon_emissions t1
                                             left join carbon_users t2 on t1.user_id = t2.id
                                         where t1.id = :id";
@@ -70,16 +70,20 @@
                                         values (:userId, :household, :transportation, :food, :totalEmission)";
 
                     $sql = $this->pdo->prepare($stmt);
-                    $sql->bindValue(":userId",         $model["userId"],  PDO::PARAM_INT);
-                    $sql->bindValue(":household",      $model["title"],   PDO::PARAM_INT);
-                    $sql->bindValue(":transportation", $model["message"], PDO::PARAM_INT);
-                    $sql->bindValue(":food",           $model["imgUrl"],  PDO::PARAM_INT);
-                    $sql->bindValue(":totalEmission",  $model["imgUrl"],  PDO::PARAM_INT);
+                    $sql->bindValue(":userId",         $model["userId"],         PDO::PARAM_INT);
+                    $sql->bindValue(":household",      $model["household"],      PDO::PARAM_INT);
+                    $sql->bindValue(":transportation", $model["transportation"], PDO::PARAM_INT);
+                    $sql->bindValue(":food",           $model["food"],           PDO::PARAM_INT);
+                    $sql->bindValue(":totalEmission",  $model["totalEmission"],  PDO::PARAM_INT);
 
-                    $sql->execute();
+                    $sql->execute();                   
+                    $lastId = $this->pdo->lastInsertId();
+
                     if($sql->rowCount() > 0) {
-                        $this->sdk->firestoreAdd('emissions', $model["userName"]);
+                        $this->sdk->firestoreAdd('emissions', $model["userId"]);
                     }
+
+                    echo json_encode(array("lastId" => $lastId));
 
                     break;
             }
--- src/utilities/Database.php
+++ src/utilities/Database.php
@@ -1,10 +1,5 @@
 <?php 
     class Database {
-        // ! // 35.186.148.24       103.3.173.137
-        // ! // carbon-db           testdb
-        // ! // root                looksee
-        // ! // Aeiou321            return2626!
-
         // This is the COMPANY one
         private $host     = '103.3.173.137';
         private $db_name  = 'testdb';
@@ -20,7 +15,6 @@
         private $conn;
         public function getPDOObject(): PDO {
             $this->conn = null;
-
             try { 
                 $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -28,25 +22,25 @@
             catch(PDOException $e) {
                 echo 'Connection Error: ' . $e->getMessage();
             }
-
             return $this->conn;
         }
 
 
         // --- 3. ==================== cloud sql via app engine ==================== --- //
-        // private $host       = '/cloudsql/rliang-carbon-project:asia-southeast1:carbon-db';
+        // private $conn;
+        // private $host       = '/cloudsql/carbon-project-9a417:asia-southeast1:carbon-db';
         // private $username   = "root";
-        // private $password   = "mycarbondb777!";
+        // private $password   = "Aeiou321";
         // private $dbname     = "carbon-db";
-        // //private $socketpath = '/cloudsql/rliang-carbon-project:asia-southeast1:carbon-db';
         
         // public function getPDOObject(): PDO {
         //     $this->conn = null;
 
         //     try { 
-        //         $connString = 'mysql:dbname={$this->dbname};unix_socket={$this->host}';
-        //         $this->conn = new PDO('mysql:dbname=carbon-db;unix_socket=/cloudsql/rliang-carbon-project:asia-southeast1:carbon-db', 
-        //                         $this->username, $this->password);
+        //         // 'mysql:dbname=carbon-db;unix_socket=/cloudsql/carbon-project-9a417:asia-southeast1:carbon-db'
+                
+        //         $connString = "mysql:dbname={$this->dbname};unix_socket={$this->host}";
+        //         $this->conn = new PDO($connString, $this->username, $this->password);
         //     } 
         //     catch(PDOException $e) {
         //         echo 'Connection Error: ' . $e->getMessage();
